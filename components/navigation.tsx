b7a81ed04108b421fd44f320c05884bdffec13ef
HannesCho <hoonsungcho11@gmail.com>
2024-03-28 22:20:56 +0100

    feat: pages

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -6,16 +6,23 @@ export default function Navigation() {
   const path = usePathname();
   return (
     <nav>
-      <ul>
+      <ul className="flex items-center justify-end p-5 gap-x-32">
         {[
           { name: "Home", href: "/" },
           { name: "About", href: "/about" },
           { name: "Skills", href: "/skills" },
           { name: "Projects", href: "/projects" },
         ].map((item: { name: string; href: string }, index) => (
-          <li key={index}>
-            <Link href={item.href}>{item.name}</Link>{" "}
-            {path === "item.href" ? "🔥" : ""}
+          <li key={index} className="flex items-center justify-center">
+            <div
+              className={
+                path === item.href
+                  ? "bg-yellow-300 px-5 py-2 rounded-full"
+                  : "px-5 py-2"
+              }
+            >
+              <Link href={item.href}>{item.name}</Link>
+            </div>
           </li>
         ))}
       </ul>
